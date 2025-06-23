9a8057c2185978982dd49a57cc1d120692c531e9
AlexandreRossi <alexandre.r.benassi@gmail.com>
2025-06-22 23:44:41 -0300

    Add files via upload

--- Exercicio/app.js
+++ Exercicio/app.js
@@ -8,20 +8,3 @@ $(document).on("click", ".botao-selecionar-exercicio", function () {
     $("#formulario-editar select[name='tipo_exercicio']").val(tipo_exercicio);
     $("#formulario-editar select[name='grupo_muscular']").val(grupo_muscular);
 });
-
-
-$(".botao-gerar-pdf").on("click", function () {
-    const element = document.body; // ou qualquer outro seletor, como: document.getElementById("seu-conteudo")
-
-    html2canvas(element).then(canvas => {
-        const imgData = canvas.toDataURL('image/png');
-        const { jsPDF } = window.jspdf;
-        const pdf = new jsPDF("p", "mm", "a4");
-        const imgProps = pdf.getImageProperties(imgData);
-        const pdfWidth = pdf.internal.pageSize.getWidth();
-        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-
-        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-        pdf.save("exercicios.pdf");
-    });
-});
